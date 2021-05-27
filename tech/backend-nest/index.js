59d2ea77235116a256c21222b961fd1bb204302d
Oleg <klimakovoleg@yandex.ru>
2021-05-27 15:07:59 +0300

    Fix paths

--- tech/backend-nest/index.js
+++ tech/backend-nest/index.js
@@ -2,22 +2,22 @@ module.exports = {
     directories: [
         {
             name: 'es-lint',
-            rule: require('src/linters/es-lint-backend'),
+            rule: require('../../src/linters/es-lint-backend'),
             path: 'src/linters/es-lint-backend',
         },
         {
             name: 'prettier',
-            rule: require('src/tools/prettier'),
+            rule: require('../../src/tools/prettier'),
             path: 'src/tools/prettier'
         },
         {
             name: 'husky',
-            rule: require('src/tools/husky'),
+            rule: require('../../src/tools/husky'),
             path: 'src/tools/husky'
         },
         {
             name: 'lint-staged',
-            rule: require('src/tools/lint-staged'),
+            rule: require('../../src/tools/lint-staged'),
             path: 'src/tools/lint-staged'
         }
     ]
