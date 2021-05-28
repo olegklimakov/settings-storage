0f7008fd7a2060be78c8d3a6049f84f44e0f7f94
Oleg <klimakovoleg@yandex.ru>
2021-05-28 20:50:01 +0300

    Added typescript settings

--- src/linters/es-lint-cli/index.js
+++ src/linters/es-lint-cli/index.js
@@ -3,6 +3,9 @@ module.exports = {
         "eslint",
         "@typescript-eslint/parser",
         "eslint-config-airbnb-base",
+        "eslint-config-airbnb-typescript",
+        "eslint-plugin-import",
+        "@typescript-eslint/eslint-plugin"
     ],
     files: [
         '.eslintrc.js',
