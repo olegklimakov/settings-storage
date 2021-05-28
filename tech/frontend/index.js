263ec865319bc57fa40a2dd6efc28802fe075117
Oleg <klimakovoleg@yandex.ru>
2021-05-28 19:34:18 +0300

    Fix path

--- tech/frontend/index.js
+++ tech/frontend/index.js
@@ -2,19 +2,19 @@ module.exports = {
     directories: [
         {
             name: 'es-lint',
-            path: '../../src/linters/es-lint',
+            path: 'src/linters/es-lint',
         },
         {
             name: 'prettier',
-            path: '../../src/tools/prettier',
+            path: 'src/tools/prettier',
         },
         {
             name: 'husky',
-            path: '../../src/tools/husky',
+            path: 'src/tools/husky',
         },
         {
             name: 'lint-staged',
-            path: '../../src/tools/lint-staged',
+            path: 'src/tools/lint-staged',
         }
     ]
 }
