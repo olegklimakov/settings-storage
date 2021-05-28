9819fe1c144502eecc05e53b7afc65a1dfb0dc89
Oleg <klimakovoleg@yandex.ru>
2021-05-28 19:08:28 +0300

    Remove require

--- tech/backend-nest/index.js
+++ tech/backend-nest/index.js
@@ -2,22 +2,18 @@ module.exports = {
     directories: [
         {
             name: 'es-lint',
-            rule: require('../../src/linters/es-lint-backend'),
             path: 'src/linters/es-lint-backend',
         },
         {
             name: 'prettier',
-            rule: require('../../src/tools/prettier'),
             path: 'src/tools/prettier'
         },
         {
             name: 'husky',
-            rule: require('../../src/tools/husky'),
             path: 'src/tools/husky'
         },
         {
             name: 'lint-staged',
-            rule: require('../../src/tools/lint-staged'),
             path: 'src/tools/lint-staged'
         }
     ]
--- tech/frontend/index.js
+++ tech/frontend/index.js
@@ -2,19 +2,19 @@ module.exports = {
     directories: [
         {
             name: 'es-lint',
-            rule: require('../../src/linters/es-lint'),
+            path: '../../src/linters/es-lint',
         },
         {
             name: 'prettier',
-            rule: require('../../src/tools/prettier'),
+            path: '../../src/tools/prettier',
         },
         {
             name: 'husky',
-            rule: require('../../src/tools/husky'),
+            path: '../../src/tools/husky',
         },
         {
             name: 'lint-staged',
-            rule: require('../../src/tools/lint-staged'),
+            path: '../../src/tools/lint-staged',
         }
     ]
 }
