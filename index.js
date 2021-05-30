bce203edc9f5c73e0ce5fa3a6bd8082022348fef
Oleg <klimakovoleg@yandex.ru>
2021-05-30 17:06:42 +0300

    Fix require

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const commonLibs = [
     {
         name: 'prettier',
-        rule: require('src/tools/prettier'),
+        path: 'src/tools/prettier',
     },
     {
         name: 'husky',
-        rule: require('src/tools/husky'),
+        path: 'src/tools/husky',
     },
     {
         name: 'lint-staged',
-        rule: require('src/tools/lint-staged'),
+        path: 'src/tools/lint-staged',
     }
 ]
 
