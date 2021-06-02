02f2e3b4d1a4f99b82155bb50082768227bf01fa
Oleg <klimakovoleg@yandex.ru>
2021-06-02 22:21:31 +0300

    Remove prettier / recommended

--- src/linters/es-lint-backend/index.js
+++ src/linters/es-lint-backend/index.js
@@ -4,7 +4,6 @@ module.exports = {
         "eslint-plugin-import@^2.22.0",
         "@typescript-eslint/eslint-plugin@^4.4.1",
         "eslint-config-prettier",
-        'prettier/recommended',
         'eslint',
         'eslint-config-airbnb-base',
         'eslint-plugin-prettier'
