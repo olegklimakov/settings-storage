642f34e48a4781ebd1d5045c4d5a59048dab59b4
Oleg <klimakovoleg@yandex.ru>
2021-06-02 22:19:06 +0300

    Remove packages

--- src/linters/es-lint-cli/index.js
+++ src/linters/es-lint-cli/index.js
@@ -4,9 +4,7 @@ module.exports = {
         "eslint-plugin-import@^2.22.0",
         "@typescript-eslint/eslint-plugin@^4.4.1",
         "eslint-config-prettier",
-        '@typescript-eslint/recommended',
         'prettier/recommended',
-        '@typescript-eslint/eslint-plugin',
         'eslint',
         'eslint-config-airbnb-base',
         'eslint-plugin-prettier'
