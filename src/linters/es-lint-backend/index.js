b21642d618840ff0b49dbc379080a1e391ad92be
Oleg <klimakovoleg@yandex.ru>
2021-06-02 22:41:49 +0300

    New settings for backend repo

--- src/linters/es-lint-backend/index.js
+++ src/linters/es-lint-backend/index.js
@@ -1,11 +1,9 @@
 module.exports = {
     devPackages: [
-        "eslint-config-airbnb-typescript",
-        "eslint-plugin-import@^2.22.0",
-        "@typescript-eslint/eslint-plugin@^4.4.1",
-        "eslint-config-prettier",
         'eslint',
-        'eslint-config-airbnb-base',
+        'eslint-plugin-import@^2.22.0',
+        '@typescript-eslint/eslint-plugin@^4.4.1',
+        'eslint-config-airbnb-typescript',
         'eslint-plugin-prettier'
     ],
     files: [
